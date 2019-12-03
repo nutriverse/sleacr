cfe1a55f10149c36a17a90d9e36daa948f005a42
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-12-03 13:36:00 +0000

    create test_classifier functions

---
+++ R/01-test_classifier.R
@@ -0,0 +1,121 @@
+################################################################################
+#
+#' Function to simulate survey data of covered/cases and non-covered/non-cases
+#' given specified parameters
+#'
+#' @param proportion A numeric value of a coverage proportion to simulate on
+#' @param pop Population size from which simulated coverage survey data is to
+#'   be taken from
+#'
+#' @return A numeric vector of cases and non-cases (1s and 0s)
+#'
+#' @examples
+#' make_data(proportion = 0.3, pop = 10000)
+#'
+#' @export
+#'
+#
+################################################################################
+
+make_data <- function(proportion, pop) {
+  d <- round(pop * (proportion / 100))
+  case <- c(rep(1, d), rep(0, pop - d))
+  id <- 1:length(case)
+  result <- data.frame(cbind(id, case))
+  return(result)
+}
+
+################################################################################
+#
+#' Function to perform LQAS based on data based on specified decision rules
+#'
+#' @param data A vector of simulated data produced by \code{make_data}
+#' @param n Sample size of actual or test coverage data
+#' @param d.lower A numeric value for the lower classification threshold
+#' @param d.upper A numeric value for the upper classification threshold
+#'
+#' @return A list of coverage proportions and LQAS outcomes
+#'
+#' @examples
+#' lqas_run(data = make_data(proportion = 0.3, pop = 10000),
+#'          n = 40, d.lower = 60, d.upper = 90)
+#'
+#' @export
+#'
+#
+################################################################################
+
+lqas_run <- function(data, n, d.lower, d.upper) {
+  ##
+  d <- c(floor(n * (d.lower / 100)), floor(n * (d.upper / 100)))
+  ##
+  survey.data <- data.frame(data[sample(x = 1:nrow(data), size = n, replace = FALSE), ])
+  d.run <- sum(survey.data$case)
+  result <- list(d = d.run, outcome = 1)
+  if (d.run > d[1])
+  {
+    result <- list(d = d.run, outcome = 2)
+  }
+  if (d.run > d[2])
+  {
+    result <- list(d = d.run, outcome = 3)
+  }
+  return(result)
+}
+
+
+################################################################################
+#
+#' Function to perform a series of LQAS analysis on simulated coverage survey
+#' data
+#'
+#' @param runs Number of simulation runs to perform; Default to 50
+#' @param pop Population size from which simulated coverage survey data is to
+#'   be taken from
+#' @param n Sample size of actual or test coverage data
+#' @param d.lower A numeric value for the lower classification threshold
+#' @param d.upper A numeric value for the upper classification threshold
+#' @param p.lower Starting proportion for simulations. Default is 0
+#' @param p.upper Ending proportion for simulations. Default is 100
+#' @param fine Granularity of simulated proportiongs; Defaul to 1
+#' @param progress Logical. Should simulation progress be shown? Default is TRUE
+#'
+#' @return A data.frame of coverage proportions and LQAS outcomes
+#'
+#' @examples
+#' lqas_simul(pop = 10000, n = 40, d.lower = 60, d.upper = 90)
+#'
+#' @export
+#'
+#
+################################################################################
+
+lqas_simul <- function(runs = 50,
+                       pop = NULL,
+                       n,
+                       d.lower,
+                       d.upper,
+                       p.lower = 0,
+                       p.upper = 100,
+                       fine = 1,
+                       progress = TRUE) {
+  ##
+  result <- data.frame()
+  ##
+  for(proportion in seq(from = p.lower, to = p.upper, by = fine)) {
+    if(progress) {
+      cat("Running simulations for proportion := ", proportion, "%\n", sep = "")
+    }
+    ##
+    test.data <- make_data(proportion, pop = pop)
+    ##
+    for(i in 1:runs) {
+      test.run <- cbind(data.frame(lqas_run(data = test.data,
+                                            n = n,
+                                            d.lower = d.lower,
+                                            d.upper = d.upper)), proportion)
+      result <- rbind(result, test.run)
+    }
+  }
+  return(result)
+}
