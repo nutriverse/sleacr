384f3e2833a0e0f4324812c77dea0c3d98c5c6ca
Ernest Guevarra <ernest@guevarra.io>
2023-01-07 18:14:07 +0000

    add test for coverage classification functions

--- tests/testthat/test-03-classify_coverage.R
+++ tests/testthat/test-03-classify_coverage.R
@@ -7,6 +7,14 @@ test_that("output is character", {
       n_in = survey_data$in_cases, n_total = survey_data$n
     ), "character"
   )
+  expect_type(
+    classify_coverage(n_in = 6, n_total = 40, standard = 0.5), "character"
+  )
+  expect_type(
+    classify_coverage(
+      n_in = survey_data$in_cases, n_total = survey_data$n, standard = 0.5
+    ), "character"
+  )
 })
 
 
