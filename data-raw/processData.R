3c8a0b1380156418f7327e715e30d747755fb599
Ernest Guevarra <ernest@guevarra.io>
2023-01-07 12:09:21 +0000

    add SLEAC data

--- data-raw/processData.R
+++ data-raw/processData.R
@@ -1,11 +1,24 @@
+## Load libraries --------------------------------------------------------------
 library(readxl)
+
+## Read and process village_list data ------------------------------------------
 
 village_list <- readxl::read_xls("data-raw/bo_village.xls", sheet = 1)
 names(village_list) <- c("id", "chiefdom", "section", "village")
 
 usethis::use_data(village_list, overwrite = TRUE, compress = "xz")
 
+## Read and process SLEAC survey data ------------------------------------------
 
+survey_data <- read.csv("data-raw/sleacSL.csv")
+
+survey_data <- subset(survey_data, select = c(-vita, -worm))
+
+names(survey_data) <- c("country", "province", "district", "in", "out", "n")
+
+survey_data <- tibble::tibble(survey_data)
+
+usethis::use_data(survey_data, overwrite = TRUE, compress = "xz")
 
 
 
